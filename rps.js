515c528fd8df10e8113116d5375be773804b2f42
James <gilford88@gmail.com>
2022-08-25 23:52:23 +0100

    Added UI to RPS

--- rps.js
+++ rps.js
@@ -1,158 +1,167 @@
+const btn1 = document.createElement('button')
+const btn2 = document.createElement('button')
+const btn3 = document.createElement('button')
+const instr = document.createElement('div')
+const displayResult = document.createElement('div')
+
+instr.textContent = "Please make your choice, first to 5 wins."
 
 
-let winner
-let aicount 
-let playercount 
-let drawcount
-
-playercount = 0
-aicount = 0
-drawcount = 0
-
-
-//Allows user to input selection
-
-function myChoice() {
-    
-    let selection = prompt("Choose your weapon!");
- 
-    let lowerSelection = selection.toLowerCase()
-    
-    if  (lowerSelection == "rock") {
-        return result = 1
-    }
-    else if ( lowerSelection == "paper") {
-        return result = 2
-    }
-    else if ( lowerSelection == "scissors") {
-        return result = 3
-    }
-    else  {
-        return result = 4
-    }
-
-    }
-    
-
-function yourChoice() {
-
-let aiInput = Math.random();
-let aiResult = aiInput.toString()[2];
+container.appendChild(instr)
+container.appendChild(btn1)
+btn1.textContent = "Rock"
+container.appendChild(btn2)
+btn2.textContent = "Paper"
+container.appendChild(btn3)
+btn3.textContent = "Scissors"
 
 
 
-    if (aiResult == 0) {
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach ((button) => {
+
+button.style.height = "40px"
+button.style.width = "150px"
+    button.addEventListener('click', () => {
+
+        select = button.textContent
+
+        myChoice(select)
+        yourChoice ()
+        playround ()
+        scoreCount ()
+
+        displayResult.textContent = `You selected ${select} and they chose ${aiNumber}. ${score}.`
+
+        container.appendChild(displayResult)
         
-        yourChoice()
+        if (aicount == 5) {
+            alert("You lose")
+            displayResult.textContent = ""
+            playercount = 0
+            aicount = 0
+        }
+        else if (playercount ==5 ) {
+            alert("You Win")
+            displayResult.textContent = ""
+            playercount = 0
+            aicount = 0            
 
-    } 
+        }
 
+})
 
-
-
-if (aiResult == 1 || aiResult == 2 || aiResult == 3) {
-    return aiNumber = 1    
-}
-else if (aiResult == 4 || aiResult ==5 || aiResult == 6) {
-    return aiNumber = 2    
-}
-else if (aiResult == 7 || aiResult == 8 || aiResult == 9) {
-    return aiNumber = 3    
-}
-
-}
-
-function playround() {
-
-myChoice()
-yourChoice()
+})
 
 
-//error log invalid input
-    if (result == 4) {
-        alert ("Please select rock, paper or scissors.")
-    } 
 
-//Draw scenarios
-    else if (result == 1 && aiNumber == 1) { 
-        alert("Rock vs rock - no winner")
-        return winner = "D"
+    let winner
+    let aicount 
+    let playercount 
+    let drawcount
+
+    playercount = 0
+    aicount = 0
+    drawcount = 0
+
+
+    //Allows user to input selection
+
+    function myChoice(choice) {
+
+        lowerSelection = choice
+        if  (lowerSelection == "Rock") {
+            return result = "Rock"
+        }
+        else if ( lowerSelection == "Paper") {
+            return result = "Paper"
+        }
+        else if ( lowerSelection == "Scissors") {
+            return result = "Scissors"
+        }
+        }
+
+
+
+    function yourChoice() {
+    let aiInput = Math.random();
+    let aiResult = aiInput.toString()[2];
+        if (aiResult == 0) {
+            yourChoice()
+        } 
+    if (aiResult == 1 || aiResult == 2 || aiResult == 3) {
+        return aiNumber = "Rock"    
     }
-    else if (result == 2 && aiNumber == 2) { 
-        alert("Paper vs paper - no winner")
-        return winner = "D"
+    else if (aiResult == 4 || aiResult ==5 || aiResult == 6) {
+        return aiNumber = "Paper"    
     }
-    else if (result == 3 && aiNumber == 3) { 
-        alert("Scissors vs Scissors - no winner")
-        return winner = "D"
+    else if (aiResult == 7 || aiResult == 8 || aiResult == 9) {
+        return aiNumber = "Scissors"    
     }
+    }
+
+
+
+    function playround() {
+    //Draw scenarios
+        if (result == "Rock" && aiNumber == "Rock") { 
+            return winner = "D"
+        }
+        else if (result == "Paper" && aiNumber == "Paper") { 
+            return winner = "D"
+        }
+        else if (result == "Scissors" && aiNumber == "Scissors") { 
+            return winner = "D"
+        }
+    //player wins scenarios
+        else if (result == "Rock" && aiNumber == "Scissors") { 
+            return winner = "P"
+        }
+        else if (result == "Paper" && aiNumber == "Rock") { 
+            return winner = "P"
+        }
+        else if (result == "Scissors" && aiNumber == "Paper") { 
+            return winner = "P"
+        }
+    //player loses scenarios
+    else if (result == "Scissors" && aiNumber == "Rock") { 
+            return winner = "ai"
+    }
+    else if (result == "Rock" && aiNumber == "Paper") { 
+            return winner = "ai"
+    }
+    else if (result == "Paper" && aiNumber == "Scissors") { 
+            return winner = "ai"
+    }
+    }
+
     
-//player wins scenarios
-    else if (result == 1 && aiNumber == 3) { 
-        alert("Rock vs Scissors - You Win")
-        return winner = "P"
-    }
-    else if (result == 2 && aiNumber == 1) { 
-        alert("Paper vs Rock - You Win")
-        return winner = "P"
-    }
-    else if (result == 3 && aiNumber == 2) { 
-        alert("Scissors vs Paper - You Win")
-        return winner = "P"
-    }
 
-//player loses scenarios
-else if (result == 3 && aiNumber == 1) { 
-    alert("Scissors vs Rock - You Lose")
-    return winner = "ai"
-}
-else if (result == 1 && aiNumber == 2) { 
-    alert("Rock vs Paper - You Lose")
-    return winner = "ai"
-}
-else if (result == 2 && aiNumber == 3) { 
-    alert("Paper vs Scissors  - You Lose")
-    return winner = "ai"
-}
-
-
-
-}
-
-function game () {
-
-
-    
-for (let i = 0; i <5; i++) {
-
-    playround()
-
+    function scoreCount () {
+      
     if (winner === "P") {
-    playercount = playercount +1
+        playercount = playercount +1
+        }
+        else if (winner === "ai") {
+        aicount = aicount +1
+        }
+        else if (winner === "D") {
+        drawcount = drawcount +1
+        }
+    
+
+
+    if (playercount == aicount) {
+        score = `It's a draw. ${drawcount} each`
+        return score
     }
-    else if (winner === "ai") {
-    aicount = aicount +1
+    else if (playercount > aicount) {
+        score = `You win! ${playercount} - ${aicount}`
+        return score
     }
-    else if (winner === "D") {
-    drawcount = drawcount +1
+    else if (playercount < aicount) {
+        score = `You lose! ${aicount} - ${playercount}`
     }
 
-}
-
-}
-
-
-
-
-game()
-
-
-if (playercount == aicount) {
-    alert (`It's a draw. ${drawcount} each`)
-}
-else if (playercount > aicount) {
-    alert (`You win! ${playercount} - ${aicount}`)
-}
-else if (playercount < aicount) {
-    alert (`You lose! ${aicount} - ${playercount}`)
 }
